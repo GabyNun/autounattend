3f9111de95c2b175bd31004e3315b93281baca18
Gabi <255643026+GabyNun@users.noreply.github.com>
2026-02-10 14:52:35 +0200

    Change oscdimg download to come from this repo

--- autounattend.ps1
+++ autounattend.ps1
@@ -10,7 +10,7 @@ Copy-Item -Path "$Drive\*" -Destination Sources -Recurse -Force
 Dismount-DiskImage -ImagePath $ISO | Out-Null
 
 irm https://github.com/GabyNun/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
-irm https://github.com/ChrisTitusTech/winutil/raw/main/releases/oscdimg.exe -Out oscdimg.exe
+irm https://github.com/GabyNun/autounattend/raw/main/oscdimg.exe -Out oscdimg.exe
 
 .\oscdimg.exe "-bSources\efi\microsoft\boot\efisys.bin" -u2 Sources autounattend.iso
 
