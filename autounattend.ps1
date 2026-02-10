aa657de4e781e44900535df2a42ff0fca5f8aa24
Gabi <255643026+GabyNun@users.noreply.github.com>
2026-02-10 14:34:48 +0200

    Update autounattend.ps1

--- autounattend.ps1
+++ autounattend.ps1
@@ -9,7 +9,7 @@ Copy-Item -Path "$Drive\*" -Destination Sources -Recurse -Force
 
 Dismount-DiskImage -ImagePath $ISO | Out-Null
 
-irm https://gist.github.com/GabiNun/81da0681f2382c90332ca771eb267500/raw/autounattend.xml -Out "Sources\autounattend.xml"
+irm https://github.com/GabyNun/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
 irm https://github.com/ChrisTitusTech/winutil/raw/main/releases/oscdimg.exe -Out oscdimg.exe
 
 .\oscdimg.exe "-bSources\efi\microsoft\boot\efisys.bin" -u2 Sources autounattend.iso
